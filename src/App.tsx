b7756c9b8c696c7a63dddfab5ce7ba5febbbf566
vvictorious <vvictorious30@gmail.com>
2021-02-08 16:49:56 -0700

    no logic yet. still setting up the html elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import QuestionCard from './components/QuestionCard'
 
-function App() {
+const App = () => {
+
+  const startTrivia = async () => {
+    
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <h1>React Quiz</h1>
+      <button className='start' onClick={startTrivia}>Start</button>
+      <p className='score'>Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className='next'>Next Question</button>
     </div>
   );
 }
