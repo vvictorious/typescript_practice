de6f2bdfa177abf4e80542c2d064ca6b3bfb925e
vvictorious <vvictorious30@gmail.com>
2021-02-08 18:55:56 -0700

    getting the data from the endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionCard from './components/QuestionCard'
+import {fetchQuizQuestions, Difficulty} from './API'
+
+
+const TOTAL_QUESTIONS = 10
 
 const App = () => {
+
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [number, setNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState([])
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true)
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
 
   const startTrivia = async () => {
     
@@ -21,7 +34,14 @@ const App = () => {
       <button className='start' onClick={startTrivia}>Start</button>
       <p className='score'>Score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard />
+      {/* <QuestionCard 
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
       <button className='next' onClick={nextQuestion}>Next Question</button>
     </div>
   );
